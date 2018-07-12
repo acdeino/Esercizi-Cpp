1ab3365855da212dd2c8980e31bf92a3dcc6e109
GithubDesktopisSeverelyBugged <ahbehnomics@gmail.com>
2018-07-12 17:33:40 +0200

    ?

--- Corso2/helloworldc2.cpp
+++ Corso2/helloworldc2.cpp
@@ -1,1 +1,17 @@
 #include <iostream>
+
+int main(){
+
+  std::cout << "Hello World! /* message */" << '\n';
+
+}
+
+
+
+ //namespace A tralalala()
+ //namespace B tralalala()
+
+ //A::tralalala()
+ //B::tralalala()
+
+ /* :: -> scope operator; cout -> character out */
