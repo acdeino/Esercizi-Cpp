b05f08a2b70ae50d15f22d15de7f6ef01b462803
GithubDesktopisSeverelyBugged <ahbehnomics@gmail.com>
2018-07-12 19:18:09 +0200

    .

--- Corso2/variables2.cpp
+++ Corso2/variables2.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
+#include <iomanip>
 
 int main () {
 
-  int year = 2018, mon = 3, day = 16;
+  int year = 2018, mon = 3, day = 6;
   //char date = Tuesday;
 
-  std::cout << "Today is " << day << mon << year << std::endl;
+  std::cout << "Today is "
+  << std::setw (2) << std::setfill ('0') << day
+  << "-"
+  << std::setw (1) << std::setfill ('0') << mon
+  << "-"
+  << std::setw (2) << year
+  << "." << std::endl;
 
   return 0;
 }
