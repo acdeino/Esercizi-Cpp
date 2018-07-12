6d2f05b9850d379908a692654186f23a24181b6f
GithubDesktopisSeverelyBugged <ahbehnomics@gmail.com>
2018-07-12 19:52:35 +0200

    .

--- Corso2/consoleprompt1.cpp
+++ Corso2/consoleprompt1.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
@@ -7,7 +8,11 @@ int main() {
   int number;
   std::cout << "Enter an integer: " << std::endl;
 
+//input in std namespace is written as follows:
   std::cin >> number;
 
+  int squared = number * number;
+  std::cout << "The squared number of your input is " << std::setw (2) << squared << std::endl;
 
+  return 0;
 }
