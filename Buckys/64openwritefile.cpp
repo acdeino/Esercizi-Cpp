ab5f759d5497c29df32cc7597254d1e5e5443e0c
GithubDesktopisSeverelyBugged <ahbehnomics@gmail.com>
2018-07-13 15:22:40 +0200

    .

---
+++ Buckys/64openwritefile.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+  ofstream buckyfile;
+  buckyfile.open("tuna.txt");
+
+  buckyfile << "I love tuna and tuna loves me! " << endl;
+  buckyfile.close();
+}
--- Corso2/constantvariables.cpp
+++ Corso2/constantvariables.cpp
@@ -7,14 +7,14 @@ using namespace std;
 int main () {
   int x = 5;
 
+// constants are defined as:
   const int y = 6;
 
   int const z = 100;
   const int w = 1;
-
-
 cout << PI << endl;
 
+// calculate the area and the perimeter of a circle with a given radius
 cout << "Enter circle radius: ";
 float radius;
 cin >> radius;
