a241d31f0218092162791802e45083d8e66ebfab
GithubDesktopisSeverelyBugged <ahbehnomics@gmail.com>
2018-07-14 20:45:10 +0200

    .

---
+++ Corso2/typesofoperators4.cpp
@@ -0,0 +1,29 @@
+// Relational operators
+
+#include <iostream>
+
+int main () {
+
+// These are relational operators:   >, >=, ==, !=, <=, <
+
+/* PLEASE NOTE: = IS NOT (!=) THE SAME AS ==;
+= is only an ASSIGNMENT operator
+== is a MATHEMATICAL RELATIONAL operator */
+
+int x = 10, y = 5, z = 15, w = 10;
+
+bool rel1 = x > y; // is 10 greater than 5? yes it is. Then the value it's true
+bool rel1 = true;
+
+bool rel2 = y > x; // is 5 greater than 10? No it isn't. Then the value is false
+bool rel2 = false;
+
+bool rel3 = x == z; // is 10 equal to 15? No it isn't. Then the value is false.
+bool rel3 = false;
+
+bool rel4 = x != z; // is 10 NOT-equal to 15? Yes it is. Then, the value is true.
+bool rel4 = true;
+
+bool rel5 = 
+
+}
