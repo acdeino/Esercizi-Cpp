54fdd88bc20cd56bf77e023d94efe7526df1238a
GithubDesktopisSeverelyBugged <ahbehnomics@gmail.com>
2018-07-14 21:39:05 +0200

    .

--- Corso2/typesofoperator5.cpp
+++ Corso2/typesofoperator5.cpp
@@ -6,10 +6,39 @@ using namespace std;
 
 int main () {
   int x = 2, y = 3;
-  if (x > 4, y < 1) {
-    std::cout << "Is 2 greater than 4? REALLY? Go back to primary school, you idiot!" << std::endl;
-
+  if (x > 4, y > 1) // it takes ONLY the second part of the construct!
+  {
+    std::cout << "Is 2 greater than 4? REALLY?" << std::endl;
   }
+
+
+
+// Conditions nesting
+
+
+if (A) {
+  some code
+  if (C) {
+    else if (D) {
+
+    }
+  }
+}
+else if (B) {
+  else if (E) {
+    else if (F){
+      
+    }
+  }
+}
+
+
+
+
+
+
+
+
 
   return 0;
 }
