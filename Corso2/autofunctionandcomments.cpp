7cfea15bcdb1c5987c6ef808bdaf44f40bc140ba
GithubDesktopisSeverelyBugged <ahbehnomics@gmail.com>
2018-07-14 18:50:35 +0200

    .

---
+++ Corso2/autofunctionandcomments.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+#define PI 3.14
+
+int main () {
+
+  auto x = 10.5;
+  auto y = 10;
+
+  int var1 = 10;
+  int var2 (20);   // these 3 are from c++11
+  int var3 {20};
+
+
+  auto var(6); //overdoing // it's not an int but behaves like a int
+
+  /* Comment
+  on more than
+   a line */
+
+  // comment on a single line
+
+
+
+}
