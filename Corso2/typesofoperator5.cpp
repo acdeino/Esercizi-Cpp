e026066a73cbd99f7f441553b0e286e808b58542
GithubDesktopisSeverelyBugged <ahbehnomics@gmail.com>
2018-07-14 21:24:00 +0200

    .

---
+++ Corso2/typesofoperator5.cpp
@@ -0,0 +1,15 @@
+// Commas operators
+
+#include <iostream>
+
+using namespace std;
+
+int main () {
+  int x = 2, y = 3;
+  if (x > 4, y < 1) {
+    std::cout << "Is 2 greater than 4? REALLY? Go back to primary school, you idiot!" << std::endl;
+
+  }
+
+  return 0;
+}
--- Corso2/typesofoperators4.cpp
+++ Corso2/typesofoperators4.cpp
@@ -12,18 +12,55 @@ int main () {
 
 int x = 10, y = 5, z = 15, w = 10;
 
-bool rel1 = x > y; // is 10 greater than 5? yes it is. Then the value it's true
-bool rel1 = true;
+bool rel1 = x > y;
+// is 10 greater than 5? yes it is. Then the value it's true
+// bool rel1 = true;
 
-bool rel2 = y > x; // is 5 greater than 10? No it isn't. Then the value is false
-bool rel2 = false;
+bool rel2 = y > x;
+// is 5 greater than 10? No it isn't. Then the value is false
+// bool rel2 = false;
 
-bool rel3 = x == z; // is 10 equal to 15? No it isn't. Then the value is false.
-bool rel3 = false;
+bool rel3 = x == z;
+ // is 10 equal to 15? No it isn't. Then the value is false.
+// bool rel3 = false;
 
-bool rel4 = x != z; // is 10 NOT-equal to 15? Yes it is. Then, the value is true.
-bool rel4 = true;
+bool rel4 = x != z;
+ // is 10 NOT-equal to 15? Yes it is. Then, the value is true.
+// bool rel4 = true;
 
-bool rel5 = 
+bool rel5 = x >= w;
+ // is 10 greater-OR-equal to 10? Yes it is. Then, the value is true.
+// bool rel5 = true;
 
+bool rel6 = x <= w;
+ /* is 10 less-OR-equal to 10? Yes it is. Then, the value is true. */
+// bool rel6 = true;
+
+bool rel7 = y >= z;
+/* is 15 greater-OR-equal to 5? No it is not.
+Then, the value is false.*/
+// bool rel7 = false;
+
+bool rel8 = !(x > w);
+/* is NOT 10 greater than 10? Yes it is, because 10 is NOT greater than 10,
+ therefore being that value false; But being the whole construct NOT, it
+ becomes NOT-false, which means is true. */
+// bool rel8 = true;
+
+bool rel9 = 12 > x > 7;
+// bool rel9 = false;
+/* because the compiler works from left to right,
+therefore considering 10 -the value of x- less than 12, which is true;
+ Having that value 'true' which the compiler assumes equal to 1,
+1 is NOT greater than 7, giving the final value of 'false'. */
+
+bool rel10 = 12 > x  && x > 7;
+/* here the compiler separates the the constructs
+at the point where the double ampersand are,
+therefore making the whole construct valid as 'true'. ( true + true = true) */
+// bool rel10 = true;
+
+
+
+return 0;
 }
