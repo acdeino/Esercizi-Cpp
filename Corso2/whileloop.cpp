59073e48414eac63c5d67525dd586039a26286d4
GithubDesktopisSeverelyBugged <ahbehnomics@gmail.com>
2018-07-18 23:42:40 +0200

    .

--- Corso2/whileloop.cpp
+++ Corso2/whileloop.cpp
@@ -7,7 +7,20 @@ int main () {
   /* while(condition) {
   code continues to be executed, until...
 } */
+ int n;
+
+ cout << "Enter a number: ";
+ cin >> n;
+
+ int sum = 0;
+ int i = 1;
+
+ while (i <= n) {
+   int number;
+   cout << ""
+ }
 
 
 
+return 0;
 }
